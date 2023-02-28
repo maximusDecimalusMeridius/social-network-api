46bb8193926c94d4f1ea028e72a0a30e040a4bbc
maximusDecimalusMeridius <106376888+maximusDecimalusMeridius@users.noreply.github.com>
2023-02-27 19:52:01 -0800

    Defined the Reaction model

--- models/Reaction.js
+++ models/Reaction.js
@@ -4,18 +4,32 @@ const mongoose = require("mongoose");
 // define mongoose schema
 const reactionSchema = new mongoose.Schema({
     reactionId: {
-
+        type: mongoose.Schema.Types.ObjectId,
+        index: true,
+        required: true,
+        auto: true
+        // or should I return a new ObjectId()?
     },
     reactionBody: {
-
+        type: String,
+        required: true,
+        max: 280
     },
     username: {
-
+        type: String,
+        required: true
     },
     createdAt: {
-
+        type: Date,
+        default: Date.now
     }
 });
+
+//getter method to format timestap
+reactionSchema.virtual("formatTimestap")
+  .get(function () {
+    return `Format the time!: ${this.createdAt}`;
+  });
 
 // declare the model object
 const Reaction = mongoose.model("Reaction", reactionSchema);
