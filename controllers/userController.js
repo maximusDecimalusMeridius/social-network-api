22ee69e5797a7194152facfee0f522e8c3e54ded
maximusDecimalusMeridius <106376888+maximusDecimalusMeridius@users.noreply.github.com>
2023-02-27 20:40:02 -0800

    Added create user and get all user functionality

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,13 +2,17 @@ const { User } = require("../models");
 
 module.exports = {
     getAllUsers(req, res) {
-        res.json("yay");
+        User.find({})
+        .then((users) => res.json(users))
+        .catch((error) => res.status(500).json(error));
     },
     getUserById(req, res) {
         res.json("yay");
     },
     createUser(req, res) {
-        res.json("yay");
+        User.create(req.body)
+        .then(res.json("User created!"))
+        .catch((error) => res.status(500).json(error));
     },
     updateUser(req, res) {
         res.json("yay");
